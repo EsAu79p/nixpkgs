d712b35e10cfcace42bbbf19e2072baaf03b21d5
EsAu79p <abi@tuta.io>
2023-11-08 23:06:28 +0100

    spectmorph: 0.5.2 -> 0.6.1

--- pkgs/applications/audio/spectmorph/default.nix
+++ pkgs/applications/audio/spectmorph/default.nix
@@ -1,16 +1,31 @@
-{ lib, stdenv, fetchurl, pkg-config, libjack2, lv2, glib, qt5, libao, cairo, libsndfile, fftwFloat }:
-
+{
+  lib,
+  stdenv,
+  fetchFromGitHub,
+  pkg-config,
+  libjack2,
+  lv2,
+  glib,
+  qt5,
+  libao,
+  cairo,
+  libsndfile,
+  fftwFloat,
+  autoreconfHook,
+}:
 stdenv.mkDerivation rec {
   pname = "spectmorph";
-  version = "0.5.2";
-  src = fetchurl {
-    url = "https://spectmorph.org/files/releases/${pname}-${version}.tar.bz2";
-    sha256 = "0yrq7mknhk096wfsx0q3b6wwa2w5la0rxa113di26rrrw136xl1f";
+  version = "0.6.1";
+  src = fetchFromGitHub {
+    owner = "swesterfeld";
+    repo = pname;
+    rev = version;
+    sha256 = "sha256-fqbVPSg7T1bVIZPhz32r1osY5df9c1VF+GEzJsyxIXo=";
   };
 
-  buildInputs = [  libjack2 lv2 glib qt5.qtbase libao cairo libsndfile fftwFloat ];
+  buildInputs = [libjack2 lv2 glib qt5.qtbase libao cairo libsndfile fftwFloat];
 
-  nativeBuildInputs = [ pkg-config ];
+  nativeBuildInputs = [pkg-config autoreconfHook];
 
   dontWrapQtApps = true;
 
@@ -18,7 +33,7 @@ stdenv.mkDerivation rec {
     description = "Allows to analyze samples of musical instruments, and to combine them (morphing) to construct hybrid sounds";
     homepage = "https://spectmorph.org";
     license = licenses.gpl3;
-    platforms = [ "x86_64-linux" "i686-linux" ];
-    maintainers = [ maintainers.magnetophon ];
+    platforms = ["x86_64-linux" "i686-linux"];
+    maintainers = [maintainers.magnetophon];
   };
 }
